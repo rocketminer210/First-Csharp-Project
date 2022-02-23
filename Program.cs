3f0c415e482e938b741ff687c1d8d47e52e2673f
Miheer <miheer1906@gmail.com>
2022-02-22 17:12:22 -0800

    First C# program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert number: ");
+            double inputted = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i <= 3; i++)
+            {
+                string mathoutput = Convert.ToString(Math.Pow(inputted, i));
+                Console.WriteLine(mathoutput);
+            }
+
+
+
+
+        }
+    }
+}
